1c281f776ab41fb4bddf3913f916ed9a8dbbdf7f
roman <roman@transkontinent.net>
2021-07-30 11:36:52 +0300

    lesson 1 homework

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/hello/{name}', function (string $name) {
     return "<h3> Hello, {$name}</h3>";
 });
 
+Route::get('/news/', function () {
+    return "<h3> Latest news !!!</h3>";
+});
+
 Route::get('/phpinfo', function () {
     return phpinfo();
 });
